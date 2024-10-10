86ff578ab90ef8876b4a7dbd04226c5369d0a307
ruser15 <nkrukov1904@gmail.com>
2024-10-10 18:36:32 +0300

    fgghdfh

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
-class ToDo:
-    def __init__(self):
+class Todo:
+    def init(self):
         self.issue = {}
         self.count = 0
-        
-    def add_issue(self, issue, clock, complete):
-        self.issue[issue] = [clock, complete]
+        self.__count_no_complete = 0
+        self.__count_transfer = 0
+    
+    def add_issue(self,issue,clock,complete,transfer):
+        self.issue[issue] = [clock,complete,transfer]
         self.count += 1
-        
-    def delete_issue(self, issue):
+    
+    def delete_issue(self,issue):
         self.issue.pop(issue)
         self.count -= 1
         
     def get_count(self):
         return self.count
+    @property
+    def count_no_complete(self):
+        return self.__count_no_complete
+    @property
+    def count_transfer(self):
+        return self.__count_transfer
+    @count_no_complete.setter
+    def count_no_complete(self,count_no_complete):
+        self.__count_no_complete = count_no_complete
+    @count_transfer.setter
+    def count_transfer(self,count_transfer):
+        self.__count_transfer = count_transfer
+        
+        
     
-    def change_issue(self, issue):
+    def change_issue(self,issue):
         temp = self.issue.get(issue)
         temp[1] = True
         self.issue.update({issue:temp})
-        
+    
+    def change_transfer(self,issue):
+        temp = self.issue.get(issue)
+        temp[2] = True
+        self.issue.update({issue:temp})
+    
     def show(self):
         nums = 1
         for key,value in self.issue.items():
-            print(f'{nums}.{key} time: {value[0]} {'Complete'if value[1] else 'Not Complete'}')
+            print(f'{nums}.{key} time:{value[0]} {'Выполнено'if value[1] else 'Не выполнено'} {'Перенесено'if value[2] else 'Не перенесено'}')
             nums += 1
             
-            
 
-todo = ToDo()
-todo.add_issue('Breakfast', '07:30', True)
-todo.add_issue('Cleaning', 'Never', False)
-todo.add_issue('Pokormit_kota', '16:00', False)
+
+todo = Todo()
+todo.add_issue('Завтрак','8:30', True,False)
+todo.add_issue('Уборка','10:00', False,False)
+todo.add_issue('Корм.Кот','12:30', False,False)
 todo.show()
-todo.change_issue('Pokormit_kota')
+todo.change_issue('Уборка')
 todo.show()
+
+todo.count_no_complete = input()
+print(todo.count_no_complete)
+#yttyty
+# написать функцию подсчета невыполненных дел и перенесенных дел
