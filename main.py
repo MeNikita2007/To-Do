9decccc440b6ab7c56b6c7167fee0563fdac59e6
ruser15 <nkrukov1904@gmail.com>
2024-10-15 21:57:58 +0300

    add update and sort dict{}

--- main.py
+++ main.py
@@ -1,67 +1,77 @@
 class Todo:
-    
-    #Конструктор класса To-Do
-    def __init__(self):
+    #Конструктор класса Todo, добавляет словарь дел и счетчики.
+    def init(self):
         self.issue = {}#Словарь дел
-        self.transfer_issue = {}#Словарь перенесённых дел
+        self.transfer_issue = {}#Словарь перенесенных дел.
         self.count = 0
-        self.__count_no_complete = 0
-        self.__count_transfer = 0
+        self.count_no_complete = 0
+        self.count_transfer = 0
     
-    #Добавляет дело
+    #Функция добавляет новое дело
     def add_issue(self,issue,clock,complete,transfer):
         self.issue[issue] = [clock,complete,transfer]
-        self.count += 1
+        #создание временного словаря и сортировка по времени
+        sort_issue = dict(sorted(self.issue.items(), key = lambda item: (int(item[1][0].split(':')[0]),int(item[1][0].split(':')[1]))))
+        #замена старого словаря на отсортированный
+        self.issue = sort_issue
+        self.init_count()
     
-    #Удаляет дело
+    #Функуия удаляет дело
     def delete_issue(self,issue):
         self.issue.pop(issue)
-        self.count -= 1
-        
-    #Возвращает общее кол-во дел
+        self.init_count()
+    
+    #Возвращает общее колличество дел    
     def get_count(self):
         return self.count
     
-    #Возвращает кол-во не выполненных дел
+    #Возвращает колличество не выполненных дел
     @property
     def count_no_complete(self):
-        return self.__count_no_complete
+        return self.count_no_complete
     
-    #Возвращает кол-во перенос дел
+    #Возвращает колличество перенесенных дел
     @property
     def count_transfer(self):
-        return self.__count_transfer
+        return self.count_transfer
     
-    #Заклад знач в кол-во не выполненных дел
+    #Закладывает значение в колличество не выполненных дел
     @count_no_complete.setter
     def count_no_complete(self,count_no_complete):
-        self.__count_no_complete = count_no_complete
+        self.count_no_complete = count_no_complete
         
-    #Заклад знач в кол-во перенос дел
+    #Закладывает значение в колличество перенесенных дел
     @count_transfer.setter
     def count_transfer(self,count_transfer):
-        self.__count_transfer = count_transfer
+        self.count_transfer = count_transfer
         
-    #Изменяет сост дела выполн\невыполн
+        
+    #Изменяет состояние дела Выполнено\Не выполнено
     def change_issue(self,issue):
         temp = self.issue.get(issue)
         if temp[1] == False:
             temp[1] = True
         else:
             temp[1] = False
+            
         self.issue.update({issue:temp})
         self.init_count()
     
-    #Изменяет сост дела перенос\неперенос
-    def change_transfer(self,issue):
+    #Изменяет состояние дела Перенесено\Не перенесено
+    def change_transfer(self,issue, new_time):
         temp = self.issue.get(issue)
         if temp[2] == False:
             temp[2] = True
+            self.transfer_issue[issue] = [new_time,False,False]#добавляем дело в новый словарь   
+            
         else:
             temp[2] = False
+            self.transfer_issue.pop(issue)#удаляем дело из нового словаря
+        
         self.issue.update({issue:temp})
         self.init_count()
-        
+    
+    #Обновляет данные счетчитков на актуальные    
     def init_count(self):
         c_complete = 0
         c_transfer = 0
@@ -75,24 +85,27 @@ class Todo:
         self.count_no_complete = c_complete
         self.count_transfer = c_transfer
         self.count = count
-    
-    #Выводит инфу о всех делах
+            
+        
+        
+    #thrth
+    #Выводит информацию о всех делах
     def show(self):
         nums = 1
         for key,value in self.issue.items():
             print(f'{nums}.{key} time:{value[0]} {'Выполнено'if value[1] else 'Не выполнено'} {'Перенесено'if value[2] else 'Не перенесено'}')
             nums += 1
-        print(f'Not completed works: {self.count_no_complete}')
-        print(f'Transfered works: {self.count_transfer}')
-            
+        print(f'Не выполненные дела: {self.count_no_complete}')
+        print(f'Перенесенные дела: {self.count_transfer}')
+        print(f'Общее колличество дел: {self.count}')
+      
 
 
 todo = Todo()
-todo.add_issue('Завтрак','8:30', True,False)
+todo.add_issue('Завтрак','8:30', False,False)
 todo.add_issue('Уборка','10:00', False,False)
 todo.add_issue('Корм.Кот','12:30', False,False)
-
-todo.change_issue('Breakfast')
-todo.change_transfer('Cleaning')
-todo.change_issue('Feed Cat')
+todo.add_issue('Ожид.Курьера','10:45', False,False)
+todo.add_issue('Починка крана','12:15', False,False)
+todo.change_transfer('Уборка', '12:50')
 todo.show()
--- main1.py
+++ main1.py
@@ -1,8 +1,9 @@
 issue = {}
 
-issue['Завтрак'] = ['8:30',False, False]
-issue['Уборка'] = ['10:30',False, False]
-issue['Корм.Кота'] = ['12:00',False, False]
+issue['Завтрак'] = ['8:00',False, False]
+issue['Корм.Кота'] = ['10:30',False, False]
+issue['Уборка'] = ['8:20',False, False]
 
-for key,value in issue.items():
+sort_issue = dict(sorted(issue.items(), key = lambda item: (int(item[1][0].split(':')[0]),int(item[1][0].split(':')[1]))))
+for key,value in sort_issue.items():
     print(key,value)
